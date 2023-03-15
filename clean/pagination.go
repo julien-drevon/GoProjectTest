a206658a37b51c2bdb43f5785d71737756ed6639
worm-rock <drevon.julien@gmail.com>
2023-03-15 19:36:01 +0100

    pagination ok

--- clean/pagination.go
+++ clean/pagination.go
@@ -10,30 +10,49 @@ type PaginationResult[TData any] struct {
 }
 
 type Pagination struct {
-	//CurrentPage int  `json:"CurrentPage"`
-	//HasNext     bool `json:"hasNext"`
-	//HasPrevious bool `json:"hasPrevious"`
-	//IsFirst     bool `json:"isFirst"`
-	//IsLast      bool `json:"isLast"`
-	TotalPage int `json:"totalPage"`
-	//PageIndex   int  `json:"pageIndex"`
-	//PageSize    int  `json:"pageSize"`
-	Total int `json:"total"`
+	CurrentPage int  `json:"CurrentPage"`
+	HasNext     bool `json:"hasNext"`
+	HasPrevious bool `json:"hasPrevious"`
+	TotalPage   int  `json:"totalPage"`
+	PageSize    int  `json:"pageSize"`
+	Total       int  `json:"total"`
 }
 
-func NewPaginationResult[TData any](values []TData, total int) PaginationResult[TData] {
+func NewPaginationResult[TData any](values []TData, total int, currentPage int, pageSizeRequest int) PaginationResult[TData] {
+	pageNumber := SetToMin1(currentPage)
 	totalPage := ComputeNbOfPage(total, len(values))
+	totalElement := SetToMin0(total)
+	pageSizeDesired := SetToMin0(pageSizeRequest)
+
 	return PaginationResult[TData]{
 		Result: values,
 		Pagination: Pagination{
-			Total:     total,
-			TotalPage: totalPage,
+			Total:       totalElement,
+			TotalPage:   totalPage,
+			CurrentPage: pageNumber,
+			HasNext:     pageNumber < totalElement,
+			HasPrevious: pageNumber > 1,
+			PageSize:    pageSizeDesired,
 		}}
 }
 
+func SetToMin0(currentPage int) int {
+	if currentPage < 1 {
+		return 0
+	}
+
+	return currentPage
+}
+func SetToMin1(currentPage int) int {
+	if currentPage < 1 {
+		return 1
+	}
+
+	return currentPage
+}
 func ComputeNbOfPage(totalEntry int, nbElementPerPage int) int {
 	if nbElementPerPage == 0 {
-		return 1
+		return 0
 	}
 
 	return int(math.Ceil(float64(float64(totalEntry) / float64(nbElementPerPage))))
--- clean/pagination_test.go
+++ clean/pagination_test.go
@@ -6,13 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestPaginationResult(t *testing.T) {
-// 	assert := assert.New(t)
-// 	result := PaginationResult[string]{}
-
-//		assert.Equal(result, nil)
-//	}
-
 func TestPaginationResult(t *testing.T) {
 	assert := assert.New(t)
 	result := PaginationResult[string]{}
@@ -22,21 +15,70 @@ func TestPaginationResult(t *testing.T) {
 
 func TestPaginationResult_New_AssignVlues(t *testing.T) {
 	assert := assert.New(t)
-	result := NewPaginationResult([]int{1}, 0)
+	result := NewPaginationResult([]int{1}, 0, 1, 2)
 	want := []int{1}
 	assert.Equal(want, result.Result)
 }
 
 func TestPaginationResult_New_TotalValue(t *testing.T) {
 	assert := assert.New(t)
-	result := NewPaginationResult([]int{1}, 42)
+	result := NewPaginationResult([]int{1}, 42, 1, 2)
 	want := 42
 	assert.Equal(want, result.Pagination.Total)
 }
 
 func TestPaginationResult_New_TotalPage(t *testing.T) {
 	assert := assert.New(t)
-	result := NewPaginationResult([]int{1, 1}, 9)
+	result := NewPaginationResult([]int{1, 1}, 9, 1, 2)
 	want := 5
 	assert.Equal(want, result.Pagination.TotalPage)
+}
+
+func TestPaginationResult_New_CurrentPage(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{1, 1}, 9, 2, 2)
+	want := 2
+	assert.Equal(want, result.Pagination.CurrentPage)
+}
+
+func TestPaginationResult_New_CurrentPageShouldBeMajorThanZero(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{1, 1}, 9, 0, 2)
+	want := 1
+	assert.Equal(want, result.Pagination.CurrentPage)
+}
+
+func TestPaginationResult_New_HasNext_whenIsTrue(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{1, 1}, 9, 2, 2)
+	want := true
+	assert.Equal(want, result.Pagination.HasNext)
+}
+
+func TestPaginationResult_New_HasPrevious_whenIsTrue(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{1, 1}, 9, 2, 2)
+	want := true
+	assert.Equal(want, result.Pagination.HasPrevious)
+}
+
+func TestPaginationResult_New_HasPageAsk_IsSet(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{1, 1}, 9, 2, 2)
+	want := 2
+	assert.Equal(want, result.Pagination.PageSize)
+}
+
+func TestPaginationResult_New_EnterBadValue(t *testing.T) {
+	assert := assert.New(t)
+	result := NewPaginationResult([]int{}, 9, 0, 0)
+	want := Pagination{
+		CurrentPage: 1,
+		HasNext:     true,
+		HasPrevious: false,
+		TotalPage:   0,
+		PageSize:    0,
+		Total:       9,
+	}
+	assert.Equal(want, result.Pagination)
 }
