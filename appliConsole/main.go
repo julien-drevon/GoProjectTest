f895358c61a9ab58ab84a02215c6b23cb20d4125
worm-rock <drevon.julien@gmail.com>
2023-03-16 13:55:02 +0100

    add choix menu

--- appliConsole/main.go
+++ appliConsole/main.go
@@ -4,18 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	AffichageMenu()
-	ChoixMenu(reader)
+
+	quit := false
+
+	for !quit {
+		reader := bufio.NewReader(os.Stdin)
+		AffichageMenu()
+		choix, _ := reader.ReadString('\n')
+		switch strings.TrimSpace(choix) {
+		case "q":
+			quit = true
+		case "1":
+			AddPokemon()
+		default:
+		}
+
+	}
+}
+func AddPokemon() {
+	fmt.Println("unimplemented")
 }
 
 func AffichageMenu() {
+
 	fmt.Println("Hello  Bienvenu sur votre pokedex!!")
 	fmt.Println("choisissez :")
 	fmt.Println("1. Ajouter un pokemon")
+	fmt.Println(("q. Quit"))
 }
 
 func ChoixMenu(reader *bufio.Reader) string {
