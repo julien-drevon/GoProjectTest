a90ddd11e92c1123284fbbdb450e063fa6515f51
worm-rock <drevon.julien@gmail.com>
2023-03-21 13:07:14 +0100

    Update repoFactory.go

--- gateway/repoFactory.go
+++ gateway/repoFactory.go
@@ -2,8 +2,6 @@ package gateway
 
 import (
 	"clean/core"
-	"encoding/json"
-	"io/ioutil"
 	"pokedex/domain"
 )
 
@@ -16,23 +14,8 @@ func NewRepoForWithPersistance(path string) (Repo, error) {
 	var err error
 
 	if core.IsExistFile(path) {
-		context, err = UnserialyzeFile[map[string][]domain.Pokemon](path)
-
+		context, err = core.UnserialyzeFile[map[string][]domain.Pokemon](path)
 	}
 
 	return Repo{Context: context, PersistFile: true, Path: path}, err
-}
-
-func UnserialyzeFile[T any](path string) (T, error) {
-	//if core.IsExistFile(path) {
-	var context T
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return context, err
-	}
-	err = json.Unmarshal(data, &context)
-
-	return context, err
-
-	//}
 }
